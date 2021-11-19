3330ab1c25cc9a0fc76e08dc7fc711697982b11a
luizrafa-federico <luizrafa.federico@gmail.com>
2021-11-19 08:25:54 -0300

    fix: routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .api_blueprint import bp as bp_api
+from .anime_blueprint import bp_animes
 
 def init_app(app: Flask):
-    app.register_blueprint(bp_api)
+    app.register_blueprint(bp_animes)
--- app/routes/api_blueprint.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from .anime_blueprint import bp_animes
-
-bp = Blueprint('bp_api', __name__, url_prefix='/api')
-
-bp.register_blueprint(bp_animes)
